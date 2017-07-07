1a23aa9f3981608dfa9a5534785acab0cebaf1b1
Gasmodius <ecorock10@gmail.com>
2017-07-07 19:01:51 +0200

    Bala tiene un rigidbody

--- Assets/Scripts/municiontm.cs
+++ Assets/Scripts/municiontm.cs
@@ -5,12 +5,8 @@ using UnityEngine;
 public class municiontm : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start (){}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update (){}
 }
