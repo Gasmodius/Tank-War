d0638eacf96e257a191abf5d0f05ef5cd31df5da
Gasmodius <ecorock10@gmail.com>
2017-07-24 14:57:49 +0200

    Sigue sin disparar bien
    He estado miarndo y parece ser que el valor de pixeles por un unidad no es 10.(tal vez haya dos valores distintos para x e y algo relacionado con la Screen)

--- Assets/Scripts/tanquescript.cs
+++ Assets/Scripts/tanquescript.cs
@@ -11,7 +11,15 @@ public class tanquescript : MonoBehaviour {
 	public float vRotar=4f;
 	private static float pixelxUnidad = 10;//Este valor esta en la Main camara y es 10
 
-	public float aux;
+	//Estas variables se usan como apoyo para ver valores------
+	public float xmouse;
+	public float ymouse;
+	public float xt;
+	public float yt;
+	public float aux_angulo_rad;
+	public float aux_angulo_gra;
+	public float auxfinal;
+	//----------------------------------------------------------
 
 	// Use this for initialization
 	void Start () {
@@ -19,14 +27,25 @@ public class tanquescript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
 
 	public void disparar(){
+		//Pruebas de valores--------------------------------------------------
+		xmouse=	Input.mousePosition.x -(Screen.width/2);
+		ymouse = Input.mousePosition.y - (Screen.height / 2);
+		xt = transform.position.x * pixelxUnidad;
+		yt=transform.position.y*pixelxUnidad;
+		aux_angulo_rad = Mathf.Atan2 ((Input.mousePosition.x - (Screen.width / 2)) - transform.position.x * pixelxUnidad, (Input.mousePosition.y - (Screen.height / 2)) - transform.position.y * pixelxUnidad);
+		aux_angulo_gra=aux_angulo_rad*Mathf.Rad2Deg;
+		//---------------------------------------------------------
+
 		float angulo = -(Mathf.Atan2((Input.mousePosition.x -(Screen.width/2)) - transform.position.x*pixelxUnidad, 
 							(Input.mousePosition.y -(Screen.height/2)) - transform.position.y*pixelxUnidad )* Mathf.Rad2Deg);
-		
+
+		auxfinal = angulo;//-----------------------------------------
+
 		cabeza.transform.rotation=Quaternion.Euler(new Vector3(transform.rotation.x, transform.rotation.y,angulo ));
 		Instantiate (municion, transform.position,cabeza.transform.rotation );
 	}
