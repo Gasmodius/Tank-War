39fabe541bef8af7fa95a5a81dcdc8a1f412659a
Gasmodius <ecorock10@gmail.com>
2017-07-24 16:55:03 +0200

    Listo para continuar
    Limpie codigo y quite objetos de prueba

--- Assets/Scripts/balascript.cs
+++ Assets/Scripts/balascript.cs
@@ -5,24 +5,22 @@ using UnityEngine;
 public class balascript : MonoBehaviour {
 
 	public float velocidad;
-	//public Vector2 direccion;
 	public int nRebotes;
-	//private Rigidbody2D rb2d;
 
 	// Use this for initialization
 	void Start () {
-		//rb2d = GetComponent<Rigidbody2D> ();
+		
 	}
 
 	// Update is called once per frame
 	void Update () {
-		//float velocidadpro = velocidad * Time.deltaTime;
 		transform.Translate (Vector2.up*velocidad);
 	}
 
-	void OnTriggerEnter2D(Collider2D elotro){
+	/*
+	void OnCollisionEnter2D(Collision2D elotro){
 		if (elotro.gameObject.tag.Equals ("ParedExterior")) {
 			Destroy (gameObject);
 		}
-	}
+	}*/
 }
