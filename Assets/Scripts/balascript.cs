a11e3ec715ff21678f7bbce1b8ad13f048e9d8f4
Gasmodius <ecorock10@gmail.com>
2017-07-30 17:28:52 +0200

    La Balas Rebotan infinito
    Las Balas ya rebotan les falta todos los comportamientos de rebotar(ejemplo: Destruir un tanque o desaparecer al rebotar mucho)

--- Assets/Scripts/balascript.cs
+++ Assets/Scripts/balascript.cs
@@ -6,21 +6,26 @@ public class balascript : MonoBehaviour {
 
 	public float velocidad;
 	public int nRebotes;
+	public Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody2D>();
+		rb.AddForce(transform.up * velocidad);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (Vector2.up*velocidad);
+
 	}
 
-	/*
+
 	void OnCollisionEnter2D(Collision2D elotro){
-		if (elotro.gameObject.tag.Equals ("ParedExterior")) {
+		if (elotro.gameObject.tag.Equals ("Bala")) {
 			Destroy (gameObject);
+
+		}else if(elotro.gameObject.tag.Equals ("Pared")){
+			nRebotes--;
 		}
-	}*/
+	}
 }
